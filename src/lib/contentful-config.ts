1bc3b225a71d7bdffb17a01d6c7b56946dfa42e8
Dmitry Moiseev <dmytro.moiseev@grammarly.com>
2025-02-14 12:11:10 +0100

    Setup

---
+++ src/lib/contentful-config.ts
@@ -0,0 +1,7 @@
+export const contentfulConfig = {
+  space: "0mqf0sdmpw0m",
+  accessToken: "iP6YA8A6vL-gA5fsrwn7pADnQq19C7yhilUk5tAIdSI",
+  environment: "master",
+  experienceTypeId: "contentfulStudio",
+  localeCode: "en-US",
+};
---
+++ src/pages/preview/[pid].tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import ErrorPage from "next/error";
+import { createClient } from "contentful";
+import {
+  ExperienceRoot,
+  detachExperienceStyles,
+  fetchBySlug,
+} from "@contentful/experiences-sdk-react";
+import Head from "next/head";
+import { defineComponents } from "@contentful/experiences-sdk-react";
+import { Button } from "@/components/experience-components/button";
+import { contentfulConfig } from "@/lib/contentful-config";
+
+// Experience components setup
+defineComponents([
+  {
+    component: Button,
+    definition: {
+      id: "custom-button",
+      name: "Button",
+      category: "Custom Components",
+      variables: {
+        text: {
+          displayName: "Text",
+          type: "Text",
+          defaultValue: "Click me",
+        },
+      },
+    },
+  },
+]);
+
+const experienceClient = createClient({
+  space: contentfulConfig.space,
+  environment: contentfulConfig.environment,
+  accessToken: contentfulConfig.accessToken,
+});
+
+interface PreviewProps {
+  experienceJSON: string | null;
+  stylesheet: string | null;
+}
+
+const Preview = (props: PreviewProps) => {
+  if (!props.experienceJSON) {
+    return <ErrorPage statusCode={404} />;
+  }
+
+  return (
+    <>
+      {props.stylesheet && (
+        <Head>
+          <style data-ssg>{props.stylesheet}</style>
+        </Head>
+      )}
+      <main style={{ width: "100%" }}>
+        <ExperienceRoot
+          experience={props.experienceJSON}
+          locale={contentfulConfig.localeCode}
+        />
+      </main>
+    </>
+  );
+};
+
+export const getServerSideProps = async ({
+  params,
+}: {
+  params?: { pid?: string };
+}) => {
+  const pid = Array.isArray(params?.pid) ? params.pid[0] : params?.pid || "";
+
+  const experience = await fetchBySlug({
+    client: experienceClient,
+    slug: pid,
+    experienceTypeId: contentfulConfig.experienceTypeId,
+    localeCode: contentfulConfig.localeCode,
+    isEditorMode: false,
+  });
+
+  const stylesheet = experience ? detachExperienceStyles(experience) : null;
+  const experienceJSON = experience ? JSON.stringify(experience) : null;
+
+  return {
+    props: {
+      experienceJSON,
+      stylesheet,
+    },
+  };
+};
+
+export default Preview;

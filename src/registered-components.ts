6c2c0e750dc4aca71e0d614416dfbf76f0e565d9
Dmitry Moiseev <dmytro.moiseev@grammarly.com>
2025-02-14 15:02:14 +0100

    fix

--- src/registered-components.ts
+++ src/registered-components.ts
@@ -1,75 +1,20 @@
-// @ts-ignore
 import { defineComponents } from "@contentful/experiences-sdk-react";
-import type { ComponentRegistration } from "@contentful/experiences-core/types";
-import { Button } from "./components/button/button";
-import { Heading } from "./components/heading/heading";
+import { Button } from "./components/button";
 
-const components: ComponentRegistration[] = [
+defineComponents([
   {
     component: Button,
     definition: {
       id: "button",
       name: "Button",
-      category: "Interactive",
+      category: "Custom Components",
       variables: {
         text: {
           displayName: "Text",
           type: "Text",
           defaultValue: "Click me!",
         },
-        variant: {
-          displayName: "Variant",
-          type: "Text",
-          defaultValue: "primary",
-          validations: {
-            in: [
-              { value: "primary" },
-              { value: "secondary" },
-              { value: "outline" },
-            ],
-          },
-        },
-        size: {
-          displayName: "Size",
-          type: "Text",
-          defaultValue: "medium",
-          validations: {
-            in: [{ value: "small" }, { value: "medium" }, { value: "large" }],
-          },
-        },
       },
     },
   },
-  {
-    component: Heading,
-    definition: {
-      id: "heading",
-      name: "Heading",
-      category: "Text",
-      variables: {
-        text: {
-          displayName: "Text",
-          type: "Text",
-          defaultValue: "Heading",
-        },
-        level: {
-          displayName: "Level",
-          type: "Text",
-          defaultValue: "h2",
-          validations: {
-            in: [
-              { value: "h1" },
-              { value: "h2" },
-              { value: "h3" },
-              { value: "h4" },
-              { value: "h5" },
-              { value: "h6" },
-            ],
-          },
-        },
-      },
-    },
-  },
-];
-
-defineComponents(components);
+]);
